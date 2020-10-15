4e4d8f8a8cab941c8c92369d9d1fd4be04f8a4f0
Your Username <you@example.com>
2020-10-15 12:29:25 -0600

    added performances controller and some views

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,2 +1,37 @@
 class PerformancesController < ApplicationController
+
+    def index
+        get_artist
+    end
+
+    def new
+        get_artist
+        @performance = Performance.new
+    end
+
+    def create
+        @artist = Artist.find_by(id: params[:performance][:artist_id])
+        @performance = Performance.new(performance_params)
+        if @performance.save
+            redirect_to artist_performances_path(@performances.artist)
+        else
+            render :new
+    end
+end
+
+
+
+
+    private
+
+    def get_artist
+        @artist ||= Artist.find_by(id: params[:artist_id])
+    end
+
+    def performance_params
+        params.require(:performance).permit(:stage_id, :duration, :description, :artist_id)
+    end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :performances, only: :create
+  resources :stages, only: :index
   resources :artists, only: [:index, :create, :new] do
     resources :performances, only: [:index, :new]
   end
-  resources :stages, only: :index
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :performances, only: :create
 end
+#   resources :performances, only: :create
+#   resources :artists, only: [:index, :create, :new] do
+#     resources :performances, only: [:index, :new]
+#   end
+#   resources :stages, only: :index
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end

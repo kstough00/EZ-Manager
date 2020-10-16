94d7c6f33827174464f2b80740b1f1369fcb44af
Your Username <you@example.com>
2020-10-16 09:35:30 -0600

    updated seed data and got stages in my db

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -1,4 +1,5 @@
 class StagesController < ApplicationController
+    before_action :set_stage, only: [:show, :edit, :update, :destroy]
 
     def index
         @stages = Stage.all
@@ -8,28 +9,37 @@ class StagesController < ApplicationController
         @stage = Stage.find(params[:id])
     end
 
-    def create
-        @stage = Stage.create(stage_params)
-    end
-
     def new
         @stage = Stage.new
     end
 
-    def large
+    def create
+        @stage = Stage.new(stage_params)
+        if @stage.save
+            redirect_to new_stage_path(@stage)
+        else
+            render :new
+        end
+    end
+
+    def stages_large
         @stages = Stage.large
         render :index
     end
 
-    def small
+    def stages_small
         @stages = Stage.small
         render :index
     end
 
     private
 
+    def set_stage
+        @stage = Stage.find(params[:id])
+    end
+
     def stage_params
-        params.require(:name).permit(:capacity, :artist_id)
+        params.require(:stage).permit(:name, :capacity)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :stages, only: [:index, :create, :new]
+  resources :stages
   resources :artists, only: [:index, :create, :new] do
     resources :performances, only: [:index, :new]
   end
@@ -7,10 +7,3 @@ Rails.application.routes.draw do
   get '/stages/large', to: 'stages#large'
   get '/stages/small', to: 'stages#small'
 end
-#   resources :performances, only: :create
-#   resources :artists, only: [:index, :create, :new] do
-#     resources :performances, only: [:index, :new]
-#   end
-#   resources :stages, only: :index
-#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-# end

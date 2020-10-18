90f71013579db8fcad886343600f7e83d7a3b01d
Your Username <you@example.com>
2020-10-18 15:12:05 -0600

    updated some CSS styling and deleted test files

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :performances, only: [:index, :new, :create]
 
   root to: 'application#home'
-  
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
